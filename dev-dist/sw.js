8bdf188cdcf02d5551b3b81104edaf21f663ef8a
Firnando Smauna <firnandosmauna@gmail.com>
2026-09-21 07:38:02 +0700

    chore: update service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f0c192c2'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.djh8l5hhpko"
+    "revision": "0.lmb2sq1h70o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
